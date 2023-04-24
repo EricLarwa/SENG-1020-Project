7d97ab127b448eb8167100d6fe6c20c0271befca
EricLarwa <larwae22@students.ecu.edu>
2023-04-24 11:00:18 -0400

    Almost Finished, Menu and Sorting done

--- Display.py
+++ Display.py
@@ -21,46 +21,59 @@ class LinkedList:
         while current_contact:
             print("Name:", current_contact.name)
             print("Phone:", current_contact.phone)
-            print("Email:", current_contact.email)
-            print("------------------")
+            print("Email:", current_contact.email, '\n')
+
             current_contact = current_contact.next
 
     def add_contact(self, contact):
-        # If the linked list is empty, set the new contact as the head
         if not self.head:
             self.head = contact
             return
 
-        # Traverse the linked list and add the new contact at the end..
         current_contact = self.head
-        while current_contact.next:
+        while current_contact.next is not None:
             current_contact = current_contact.next
-
         current_contact.next = contact
 
     def remove_contact(self, contact):
-        # If the contact to be removed is the head, update the head :o
-        if contact == self.head:
-            self.head = contact.next
+        if self.head is None:
+            print(f"{contact} was not found")
             return
 
-        # Traverse the linked list and remove the contact
-        predecessor = None
-        current_contact = self.head
-        while current_contact:
-            if current_contact == contact:
-                predecessor.next = current_contact.next
-                return
-            predecessor = current_contact
-            current_contact = current_contact.next
+        successor = contact.next
+        predecessor = contact.prev
+        while successor is not None:
+            if successor != None:
+                successor.prev = predecessor
+            if predecessor != None:
+                predecessor.next = successor
+            if contact is self.head:
+                self.head = successor
 
     def search_contact(self, name):
-        # Traverse the linked list and return the contact with the matching name *
         current_contact = self.head
-        while current_contact:
+        while current_contact is not None:
             if current_contact.name == name:
-                return current_contact
+                print(f"{name} ")
+                print(f"{current_contact.phone} ")
+                print(f"{current_contact.email}",'\n')
+                return
             current_contact = current_contact.next
+        print(f"{name} is not in the Contact Book")
+    def sort_list(self):
+        if self.head is None:
+            print("The Contact Book Is Empty")
+        current = self.head
+        contact_list = []
+        while current is not None:
+            contact_list.append(current)
+            current = current.next
 
-        # If the contact is not found, return None :(
-        return None
+        sorted_list = sorted(contact_list, key=lambda x: x.name)
+        self.head = sorted_list[0]
+        current = self.head
+        for contact in sorted_list[1:]:
+            current.next = contact
+            current = current.next
+        current.next = None
+        print("The Contacts have been sorted by name")
--- Main.py
+++ Main.py
@@ -1,57 +1,38 @@
 from Display import Contact
+from Display import LinkedList
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def display(self):
-        if not self.head:
-            print("No contact found")
-        #FIXme (Continue)
-
-    def add_contact(self, name, phone, email):
-          #FIXme
-
-    def remove_contact(self, contact):
-        successor = contact.next
-        predecessor = contact.prev
-        if successor != None:
-            successor.prev = predecessor
-        if predecessor != None:
-            predecessor.next = successor
-        if contact is self.head:
-            self.head = successor
-        if contact is self.tail:
-            self.tail = predecessor
-
-    def search_contact(self, search_contact):
-        current_contact = self.head
-        while current_contact != None:
-            if current_contact == search_contact:
-                return current_contact
-            else:
-                current_contact = current_contact.next
-
-        return None
 
 Contact_Book = LinkedList()
 while True:
-    print("Enter 1 to add a new contact")
-    print("Enter 2 to display all contacts")
-    print("Enter 3 to search contacts")
+    print("Enter 1 to Add a new Contact")
+    print("Enter 2 to Display all Contacts")
+    print("Enter 3 to Search Contacts")
+    print("Enter 4 to Remove a Contact")
+    print("Enter 5 to Sort Contacts")
+    print("Enter 6 to Exit")
 
     choice = int(input("Enter your choice:"))
     print("                                ")
     if choice == 1:
-        name = input("Enter the name:")
-        phone = input("Enter phone number:")
-        email = input("Enter the email address:")
+        name = input("Enter the name: ")
+        phone = input("Enter phone number: ")
+        email = input("Enter the email address: ")
         contact = Contact(name,phone,email)
         Contact_Book.add_contact(contact)
         print("Contact added",'\n')
     elif choice == 2:
         Contact_Book.display()
     elif choice == 3:
+        name = input("Enter the name of the contact you are searching for: ")
+        print("                         ")
+        Contact_Book.search_contact(name)
+    elif choice == 4:
+        name = input("Enter the name of the contact to be removed: ")
+        print("                         ")
+        Contact_Book.remove_contact(contact)
+    elif choice == 5:
+        Contact_Book.sort_list()
+    elif choice == 6:
         print("Exiting...")
         break
     else:
